b4224d093632087f295400bc544fd21c75324f75
Meyson Freitas <meyson6603@gmail.com>
2025-03-03 18:19:43 -0300

    Add package bcrypt for password hashing in login and registration

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 // const { json } = require('express')
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
+const bcrypt = require('bcrypt');
+// const { use } = require('../router/users');
 
 const keySecrect = 'kdjkfljsadklfjsdalkjaslkdjsalkdjs'
 const users = [{ id: 1, username: 'Meyson', email: 'meyson6603@gmail.com', password: '123456', role: 'admin' }, { id: 2, username: 'Vitor', email: 'vitor@gmail.com', password: '123456', role: 'admin' }]
@@ -23,10 +25,10 @@ function getUsers(req, res) {
     // console.log(loginUser)
 }
 
-function registerUsers(req, res) {
+async function registerUsers(req, res) {
     const username = req.body.username
     const email = req.body.email
-    const password = req.body.password
+    const password = await hashPassword(req.body.password)
 
     users.forEach(user => {
         if (user.email == email) {
@@ -48,30 +50,22 @@ function registerUsers(req, res) {
     // res.send('Adicionado com Sucesso')
 }
 
-function loginUsers(req, res) {
+async function loginUsers(req, res) {
     const email = req.body.email
     const password = req.body.password
-    console.log(email, password)
-    // const { inputUsername, inputPassword } = req.params
-
-    const loginUser = users.find(user => user.email == email && user.password == password)
-    if (loginUser) {
-        const createJWT = jwt.sign({ username: loginUser.username, email: loginUser.email, role: loginUser.role }, keySecrect)
-        res.cookie('session', createJWT)
-        return res.json(loginUser)
-    } else {
-        console.log(loginUser)
-        return res.status(401).send('Usuario ou senha não encontrados')
+    const loginUser = users.find(user => user.email == email);
+    if (!loginUser) {
+        return res.status(401).send('Usuário não encontrado');
     }
-    // console.log(loginUser)
 
-    // res.json(loginUser)
-
-    // const singJWT = jwt.sing({ username: username, email: email, })
-
-    // console.log('teste')
-    // console.log(email)
-    // res.json({ message: 'Deu bom' })
+    const isPasswordValid = await verifyPassword(password, loginUser.password);
+    if (isPasswordValid) {
+        const createJWT = jwt.sign({ username: loginUser.username, email: loginUser.email, role: loginUser.role }, keySecrect);
+        res.cookie('session', createJWT);
+        return res.json(loginUser);
+    } else {
+        return res.status(401).send('Senha incorreta');
+    }
 }
 
 function logout(req, res) {
@@ -81,3 +75,34 @@ function logout(req, res) {
 }
 
 module.exports = { getUsers, registerUsers, loginUsers, logout }
+
+
+
+
+async function hashPassword(password) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+}
+
+// Função para verificar se a senha informada é válida
+async function verifyPassword(inputPassword, storedHashedPassword) {
+    const isMatch = await bcrypt.compare(inputPassword, storedHashedPassword);
+    return isMatch;  // Retorna true se as senhas coincidirem, ou false caso contrário
+}
+
+// // Exemplo de uso
+// async function example() {
+//     const password = 'senhaSegura123';
+
+//     // Gerar o hash da senha
+//     const hashedPassword = await hashPassword(password);
+//     console.log('Senha armazenada (hash):', hashedPassword);
+
+//     // Verificar a senha
+//     const isCorrect = await verifyPassword('senhaSegura123', hashedPassword);
+//     console.log('Senha correta?', isCorrect);  // true
+
+//     const isIncorrect = await verifyPassword('senhaErrada123', hashedPassword);
+//     console.log('Senha correta?', isIncorrect);  // false
+// }
